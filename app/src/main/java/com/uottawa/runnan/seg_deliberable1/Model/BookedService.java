6cae4a91930fd8f05e1097d8372093b4b9d48592
Dolores <dolores@RunnandeMacBook-Air.local>
2018-12-03 23:59:51 -0500

    change

---
+++ app/src/main/java/com/uottawa/runnan/seg_deliberable1/Model/BookedService.java
@@ -0,0 +1,55 @@
+package com.uottawa.runnan.seg_deliberable1.Model;
+
+public class BookedService {
+    public BookedService(String hoName, String spName, String id) {
+        this.hoName = hoName;
+        this.spName = spName;
+        this.id = id;
+    }
+    public BookedService(String hoName, String spName, String service,String id) {
+        this.hoName = hoName;
+        this.spName = spName;
+        this.id = id;
+        this.service = service;
+    }
+    public BookedService(){};
+
+    public String getHoName() {
+        return hoName;
+    }
+
+    public void setHoName(String hoName) {
+        this.hoName = hoName;
+    }
+
+    public String getSpName() {
+        return spName;
+    }
+
+    public void setSpName(String spName) {
+        this.spName = spName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    private String hoName;
+    private String spName;
+    private String id;
+    private String service;
+
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+}
